3b369e62538c1c7ef3df7ceb9e4e8d9a3a485b63
Abmael <abmael@localhost.localdomain>
2022-02-17 15:15:21 -0300

    renaming variables and funcs and commenting

--- python3/agent.py
+++ python3/agent.py
@@ -7,7 +7,7 @@ import os
 uri = os.environ.get(
     'GAME_CONNECTION_STRING') or "ws://127.0.0.1:3000/?role=agent&agentId=agentId&name=defaultName"
 
-actions = ["up", "down", "left", "right", "bomb", "detonate"]
+acoes = ["up", "down", "left", "right", "bomb", "detonate"]
 
 
 class Agent():
@@ -25,42 +25,48 @@ class Agent():
         loop.run_until_complete(asyncio.wait(tasks))
 
     # returns coordinates of the first bomb placed by a unit
-    def _get_bomb_to_detonate(self, unit) -> Union[int, int] or None:
-        entities = self._client._state.get("entities")
-        bombs = list(filter(lambda entity: entity.get(
-            "unit_id") == unit and entity.get("type") == "b", entities))
-        bomb = next(iter(bombs or []), None)
+    # retorna coordenadas da primeira bomba plantada por uma unidade
+    def _obter_coordenada_de_bomba_para_detonar(self, unit) -> Union[int, int] or None:
+        entidades = self._client._state.get("entities")
+        bombas = list(filter(lambda entity: entity.get(
+            "unit_id") == unit and entity.get("type") == "b", entidades))
+        bomb = next(iter(bombas or []), None)
         if bomb != None:
             return [bomb.get("x"), bomb.get("y")]
         else:
             return None
 
-    async def _on_game_tick(self, tick_number, game_state):
+    # fazer o "cérebro" do agente aqui por fora, e tomada de decisão
+    # fazer o A*
+    # usar o A* para definir uma função que retorne o melhor posicionamento para a unidade no momento de explosao
+    # essa func recebe o x e y da unidade e o x e y da bomba 
+    # atualmente é feito por if(x !=agent_x or x !=agent_x+1 or y != agent_y or y != agent_y+1):
 
+    async def _on_game_tick(self, tick_number, game_state):
         # get my units
+        # recuperando os agentes/unidades
         my_agent_id = game_state.get("connection").get("agent_id")
         my_units = game_state.get("agents").get(my_agent_id).get("unit_ids")
 
-        # send each unit a random action
-
-        
+        # send each unit a random acao
+        # tomada de decisão de cada unidade
         for unit_id in my_units:
-
-            action = random.choice(actions)
-
-            if action in ["up", "left", "right", "down"]:
-                await self._client.send_move(action, unit_id)
-            elif action == "bomb":
+            acao = random.choice(acoes)
+            if acao in ["up", "left", "right", "down"]:
+                await self._client.send_move(acao, unit_id)
+            elif acao == "bomb":
                 await self._client.send_bomb(unit_id)
-            elif action == "detonate":
-                bomb_coordinates = self._get_bomb_to_detonate(unit_id)
-                if bomb_coordinates != None:
-                    x, y = bomb_coordinates
+
+            # quando for detonar, escolher melhor lugar para ir e o melhor momento para detonar
+            elif acao == "detonate":
+                bomba_coordenadas = self._obter_coordenada_de_bomba_para_detonar(unit_id)
+                if bomba_coordenadas != None:
+                    x, y = bomba_coordenadas
                     agent_x, agent_y = unit_id.get('coordinates')
                     if(x !=agent_x or x !=agent_x+1 or y != agent_y or y != agent_y+1):
                         await self._client.send_detonate(x, y, unit_id)
             else:
-                print(f"Unhandled action: {action} for unit {unit_id}")
+                print(f"Unhandled acao: {acao} for unit {unit_id}")
 
 
 def main():
